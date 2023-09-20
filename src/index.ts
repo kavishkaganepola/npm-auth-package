eaaff8e7a86f4f09d3cf4116140ddab23494c673
kavishkaG <kavishka.ganepola@lunatech.dev>
2023-09-20 17:12:12 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import { Redis } from 'ioredis';
 
-export async function redisConnection(
-  REDIS_HOST: string,
-  REDIS_PORT: string,
-  REDIS_PASSWORD: string,
-  access_token: string
-) {
-  const redisClient = new Redis({
-    host: REDIS_HOST,
-    port: REDIS_PORT,
-    password: REDIS_PASSWORD,
-  });
-  redisClient.sadd('auth_redis_set', access_token);
-  return access_token;
+export class authentication {
+  async redisConnection(
+    REDIS_HOST: string,
+    REDIS_PORT: string,
+    REDIS_PASSWORD: string,
+    access_token: string
+  ) {
+    const redisClient = new Redis({
+      host: REDIS_HOST,
+      port: REDIS_PORT,
+      password: REDIS_PASSWORD,
+    });
+    redisClient.sadd('auth_redis_set', access_token);
+    return access_token;
+  }
 }
