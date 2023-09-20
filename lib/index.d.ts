c7f96adc4d1c85009c8d9f7788e6772041c093ca
kavishkaG <kavishka.ganepola@lunatech.dev>
2023-09-20 17:13:54 +0530

    added class

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
-export declare function redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string, access_token: string): string;
+export declare class authentication {
+    redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string, access_token: string): Promise<string>;
+}
 //# sourceMappingURL=index.d.ts.map
