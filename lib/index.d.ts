d0cc4af06bb2095091bce05616e24b77ee9b88b2
kavishkaG <kavishka.ganepola@lunatech.dev>
2023-09-20 16:27:43 +0530

    added redis integration

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export declare function redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string, access_token: string): string;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Redis } from 'ioredis';
+
+export function redisConnection(
+  REDIS_HOST: string,
+  REDIS_PORT: string,
+  REDIS_PASSWORD: string,
+  access_token: string
+) {
+  const redisClient = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD,
+  });
+  redisClient.sadd('auth_redis_set', access_token);
+  return access_token;
+}
