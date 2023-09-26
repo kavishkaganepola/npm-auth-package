3697b31ee2e5ed84652805895a80080392eccb56
kavishkaG <kavishka.ganepola@lunatech.dev>
2023-09-26 13:39:34 +0530

    1

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,6 @@
-export declare class Authentication {
-    redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string, access_token: string): Promise<string>;
+declare class Authentication {
+    constructor();
+    static redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string, access_token: string): string;
 }
+export default Authentication;
 //# sourceMappingURL=index.d.ts.map
