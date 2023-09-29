3858b2c14b67f442d4578847c30093b7dc0921bb
kavishkaG <kavishka.ganepola@lunatech.dev>
2023-09-29 11:52:38 +0530

    0.0.9

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,7 @@
+import { Redis } from 'ioredis';
+import { DataSource } from 'typeorm';
 export declare class Authentication {
-    redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string, access_token: string): string;
+    redisConnection(REDIS_HOST: string, REDIS_PORT: string, REDIS_PASSWORD: string): Redis;
+    mysqlDBConnection(DB_HOST: string, DB_PORT: number, DB_USERNAME: string, DB_PASSWORD: string, DB_NAME: string): DataSource;
 }
 //# sourceMappingURL=index.d.ts.map
